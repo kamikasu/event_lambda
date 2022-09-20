4b1ba6bccdc6d7baf65128f2c49dd05f77c9f781
kamikasu <italo.mendoza@wiedii.co>
2022-09-20 14:36:23 -0500

    ajuste socket io in lambda functions on aws

--- eventLambda/index.js
+++ eventLambda/index.js
@@ -1,4 +1,7 @@
 exports.handler = async (event) => {
+    console.log('siuuuuuuuuu');
+    console.log('kamikasu',event);
+    console.log('xxxx',JSON.stringify(event, null, 2));
     var io = require('socket.io-client');
     var socket = io('http://ec2-3-93-179-92.compute-1.amazonaws.com:3100', {query: {
         token: 'BACKENDCLIENTSOCKET'
