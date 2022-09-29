9cf7c828ceba4e47d396d17171abc09d6a739c1c
kamikasu <italo.mendoza@wiedii.co>
2022-09-29 02:11:44 -0500

    clean data and emit data by socket to aplications dashboard

--- eventLambda/index.js
+++ eventLambda/index.js
@@ -1,14 +1,29 @@
-exports.handler = async (event) => {
-    console.log('siuuuuuuuuu');
-    console.log('kamikasu',event);
-    console.log('xxxx',JSON.stringify(event, null, 2));
-    var io = require('socket.io-client');
-    var socket = io('http://ec2-3-93-179-92.compute-1.amazonaws.com:3100', {query: {
-        token: 'BACKENDCLIENTSOCKET'
-    }});
+exports.handler = (event,context,callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    var data_import= event.detail.fullDocument;
+    delete data_import['parameters'];
+    delete data_import['agente'];
+    delete data_import['_id'];
+    delete data_import['script'];
+    // var data_export=JSON.stringify(event);
+    var type_chart='default';
+    if (data_import['module'] == 'Ticket' && data_import['action'] == 'Accept' ){
+        type_chart='accept_ticket'
+    }
     
-    let dataset = { "id": "1" };
-    socket.emit("MyEvent", JSON.stringify(event, null, 2));
 
-    return 'Sent message!';
-  };
+    var io = require('socket.io-client');
+    var socket = io('http://ec2-3-93-179-92.compute-1.amazonaws.com:3100', {
+        transports: ['websocket','polling'],
+        query: {
+            token: 'BACKENDCLIENTSOCKET'
+        }
+    });
+    socket.on("connect", () => {
+        socket.emit('eventsApiBackend', {event: type_chart, data: data_import},function (params) {
+            socket.disconnect();
+            callback(null, 200);
+        });
+        
+    });
+};
